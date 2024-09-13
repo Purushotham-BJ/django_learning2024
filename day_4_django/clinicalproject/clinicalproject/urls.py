4452bcd82086cdb4c16cb21c3455827c16746bca
Purushotham-BJ <purushothamabj883@gmail.com>
2024-09-13 11:51:28 +0530

    patientapp programs added

--- day_4_django/clinicalproject/clinicalproject/urls.py
+++ day_4_django/clinicalproject/clinicalproject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('doctors/', include('DoctorApp.urls')),
+    path('patients/', include('PatientApp.urls')),
 ]
