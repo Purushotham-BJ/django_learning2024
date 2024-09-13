6e147b5cc160f79c7ddb52105b722e27dc4dc8e3
Purushotham-BJ <purushothamabj883@gmail.com>
2024-09-13 12:03:01 +0530

    urls changed

--- day_4_django/clinicalproject/clinicalproject/urls.py
+++ day_4_django/clinicalproject/clinicalproject/urls.py
@@ -20,9 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('doctors/', include('DoctorApp.urls')),
-]
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
     path('patients/', include('PatientApp.urls')),
 ]
